a5789f4b3bdcc456bda6d32f6f7182705e6aaaba
A. G. Pramita <155745029+Pramita03@users.noreply.github.com>
2025-12-08 19:35:46 +0700

    Rename migration class and update table structure

--- app/Database/Migrations/create_mata_kuliah.php
+++ app/Database/Migrations/create_mata_kuliah.php
@@ -1,27 +1,42 @@
 <?php
+
 namespace App\Database\Migrations;
+
 use CodeIgniter\Database\Migration;
 
-
-class CreateMataKuliah extends Migration
+class Matakuliah extends Migration
 {
-public function up()
-{
-$this->forge->addField([
-'id_mk' => ['type'=>'INT','constraint'=>11,'unsigned'=>true,'auto_increment'=>true],
-'kode_mk' => ['type'=>'VARCHAR','constraint'=>20,'null'=>false],
-'nama_mk' => ['type'=>'VARCHAR','constraint'=>100,'null'=>false],
-'sks' => ['type'=>'INT','constraint'=>3,'null'=>false],
-'created_at' => ['type'=>'DATETIME','null'=>true],
-'updated_at' => ['type'=>'DATETIME','null'=>true],
-]);
-$this->forge->addKey('id_mk', true);
-$this->forge->createTable('mata_kuliah');
-}
+    public function up()
+    {
+        // Tabel matakuliah dibuat sederhana saja
+        $this->forge->addField([
+            'id' => [
+                'type' => 'INT',
+                'unsigned' => true,
+                'auto_increment' => true
+            ],
+            'kode' => [
+                'type' => 'VARCHAR',
+                'constraint' => 20
+            ],
+            'nama_mk' => [
+                'type' => 'VARCHAR',
+                'constraint' => 100
+            ],
+            'sks' => [
+                'type' => 'INT',
+                'constraint' => 2
+            ],
+            'created_at DATETIME NULL',
+            'updated_at DATETIME NULL'
+        ]);
 
+        $this->forge->addKey('id', true);
+        $this->forge->createTable('matakuliah');
+    }
 
-public function down()
-{
-$this->forge->dropTable('mata_kuliah');
-}
+    public function down()
+    {
+        $this->forge->dropTable('matakuliah');
+    }
 }
