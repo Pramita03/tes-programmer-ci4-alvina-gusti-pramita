5af335b0aff85b883aa57a10520fae51b5ef717b
A. G. Pramita <155745029+Pramita03@users.noreply.github.com>
2025-12-08 19:35:19 +0700

    Update create_mahasiswa.php

--- app/Database/Migrations/create_mahasiswa.php
+++ app/Database/Migrations/create_mahasiswa.php
@@ -1,36 +1,53 @@
 <?php
-namespace App\Database\Migrations;
 
+namespace App\Database\Migrations;
 
 use CodeIgniter\Database\Migration;
 
-
-class CreateMahasiswa extends Migration
+class Mahasiswa extends Migration
 {
-public function up()
-{
-$this->forge->addField([
-'id_mahasiswa' => ['type'=>'INT','constraint'=>11,'unsigned'=>true,'auto_increment'=>true],
-'nim' => ['type'=>'VARCHAR','constraint'=>20,'null'=>false],
-'nama' => ['type'=>'VARCHAR','constraint'=>100,'null'=>false],
-'jenis_kelamin' => ['type'=>'ENUM','constraint'=>"'L','P'",'null'=>false],
-'tempat_lahir' => ['type'=>'VARCHAR','constraint'=>100,'null'=>true],
-'tanggal_lahir' => ['type'=>'DATE','null'=>true],
-'alamat' => ['type'=>'TEXT','null'=>true],
-'no_hp' => ['type'=>'VARCHAR','constraint'=>20,'null'=>true],
-'email' => ['type'=>'VARCHAR','constraint'=>100,'null'=>true],
-'foto' => ['type'=>'VARCHAR','constraint'=>255,'null'=>true],
-'created_at' => ['type'=>'DATETIME','null'=>true],
-'updated_at' => ['type'=>'DATETIME','null'=>true],
-]);
-$this->forge->addKey('id_mahasiswa', true);
-$this->forge->addKey('nim');
-$this->forge->createTable('mahasiswa');
-}
+    public function up()
+    {
+        // Struktur tabel mahasiswa
+        // Catatan: isian dibuat yang wajar dipakai kampus
+        $this->forge->addField([
+            'id' => [
+                'type' => 'INT',
+                'unsigned' => true,
+                'auto_increment' => true
+            ],
+            'nim' => [
+                'type' => 'VARCHAR',
+                'constraint' => 20
+            ],
+            'nama' => [
+                'type' => 'VARCHAR',
+                'constraint' => 100
+            ],
+            'jenis_kelamin' => [
+                'type' => 'ENUM',
+                'constraint' => ['L', 'P']
+            ],
+            'email' => [
+                'type' => 'VARCHAR',
+                'constraint' => 100,
+                'null' => true
+            ],
+            'foto' => [
+                'type' => 'VARCHAR',
+                'constraint' => 255,
+                'null' => true
+            ],
+            'created_at DATETIME NULL',
+            'updated_at DATETIME NULL'
+        ]);
 
+        $this->forge->addKey('id', true);
+        $this->forge->createTable('mahasiswa');
+    }
 
-public function down()
-{
-$this->forge->dropTable('mahasiswa');
-}
+    public function down()
+    {
+        $this->forge->dropTable('mahasiswa');
+    }
 }
