0f37174a3e186f0fac2c60d078f48766cebaa261
A. G. Pramita <155745029+Pramita03@users.noreply.github.com>
2025-12-14 09:54:25 +0700

    Add sidebar layout for navigation

---
+++ app/Views/layout/sidebar.php
@@ -0,0 +1,26 @@
+<div class="bg-dark text-white p-3" style="width:260px; min-height:100vh">
+    <h5 class="mb-4"><i class="fa-solid fa-graduation-cap"></i> Akademik</h5>
+
+    <ul class="nav nav-pills flex-column gap-2">
+        <li class="nav-item">
+            <a href="/mahasiswa" class="nav-link text-white">
+                <i class="fa fa-users"></i> Mahasiswa
+            </a>
+        </li>
+        <li class="nav-item">
+            <a href="/matakuliah" class="nav-link text-white">
+                <i class="fa fa-book"></i> Mata Kuliah
+            </a>
+        </li>
+        <li class="nav-item">
+            <a href="/krs" class="nav-link text-white">
+                <i class="fa fa-list"></i> KRS
+            </a>
+        </li>
+        <li class="nav-item mt-4">
+            <a href="/logout" class="nav-link text-danger">
+                <i class="fa fa-sign-out-alt"></i> Logout
+            </a>
+        </li>
+    </ul>
+</div>
