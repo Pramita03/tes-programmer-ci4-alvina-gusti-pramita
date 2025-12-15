57dd436bc67a271cbda4a8760c6d184c708909b0
A. G. Pramita <155745029+Pramita03@users.noreply.github.com>
2025-12-15 19:38:30 +0700

    Update create_mata_kuliah.php

--- app/Database/Migrations/create_mata_kuliah.php
+++ app/Database/Migrations/create_mata_kuliah.php
@@ -4,14 +4,15 @@ namespace App\Database\Migrations;
 
 use CodeIgniter\Database\Migration;
 
-class Matakuliah extends Migration
+class CreateMatakuliahTable extends Migration
 {
     public function up()
     {
-        // Tabel matakuliah dibuat sederhana saja
+        // Struktur tabel mata kuliah
         $this->forge->addField([
             'id' => [
                 'type' => 'INT',
+                'constraint' => 11,
                 'unsigned' => true,
                 'auto_increment' => true
             ],
@@ -32,6 +33,7 @@ class Matakuliah extends Migration
         ]);
 
         $this->forge->addKey('id', true);
+        $this->forge->addUniqueKey('kode'); // kode MK sebaiknya unik
         $this->forge->createTable('matakuliah');
     }
 
